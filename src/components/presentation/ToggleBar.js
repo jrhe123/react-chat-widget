6aa6b5087d6078b7eca218136931f7a3f5bdcf63
jrhe123 <keith_he@outlook.com>
2017-08-05 00:41:47 -0400

    change iamge

--- src/components/presentation/ToggleBar.js
+++ src/components/presentation/ToggleBar.js
@@ -12,7 +12,7 @@ class ToggleBar extends Component{
 
 	render(){
 
-		let imgUrl = this.props.toggle ? 'http://res.cloudinary.com/dqtdsrmtz/image/upload/v1501814805/qezte6sbolkfymefc5lw.gif' : 'http://res.cloudinary.com/dqtdsrmtz/image/upload/v1501814811/wwzvaftnzxljfjjunylg.png';
+		let imgUrl = this.props.toggle ? 'http://res.cloudinary.com/dqtdsrmtz/image/upload/v1501907887/mctadhunj3incpzld2bs.png' : 'http://res.cloudinary.com/dqtdsrmtz/image/upload/v1501907891/tmdsa8khyhzzkruwamw7.png';
 		let borderRadius = this.props.toggle ? '50%' : '13px';
 		const divStyle = {
             backgroundImage: 'url(' + imgUrl + ')',
